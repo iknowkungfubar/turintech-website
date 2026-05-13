8c72c3cd275151bb149a143c950c028c50269395
Turin <turin.ortherion@gmail.com>
2026-05-13 03:00:43 -0700

    refactor: restructure project for Astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.turintechsolutions.com',
+  build: {
+    format: 'directory',
+  },
+});
