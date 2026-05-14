523fad61bd4b7177a1296df78b2104bfee342832
Turin <turin.ortherion@gmail.com>
2026-05-13 21:55:56 -0700

    fix: remove base path from astro config - links should be root-relative for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://turintechsolutions.com',
-  base: '/turintech-website',
   build: {
     format: 'directory',
   },
